7a37a6ef197079cd63a0d1bebd5afeddda5923dc
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2022-01-15 08:46:00 -0500

    OI updates
    Insures the robot uses joysticks not gamepad and also add initButtons in OI.

--- src/main/java/org/frc5687/rapidreact/OI.java
+++ src/main/java/org/frc5687/rapidreact/OI.java
@@ -1,64 +1,40 @@
-/* Team 5687 (C)2020-2021 */
+/** Team 5687 (C)2021-2022
+ * Joystick and gamepad control for the robot.
+ * Also has button inits.
+ * Has some instructions on how to switch controls.
+*/
 package org.frc5687.rapidreact;
 
 import static org.frc5687.rapidreact.util.Helpers.*;
-
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.Button;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import org.frc5687.rapidreact.subsystems.DriveTrain;
-import org.frc5687.rapidreact.util.AxisButton;
 import org.frc5687.rapidreact.util.Gamepad;
 import org.frc5687.rapidreact.util.OutliersProxy;
 
 public class OI extends OutliersProxy {
-    protected Gamepad _driverGamepad;
-    protected Joystick _leftJoystick;
-    protected Joystick _rightJoystick;
-
-    protected Button _driverRightStickButton;
-
-    private JoystickButton _trigger;
-    private JoystickButton _thumbButton;
-    private JoystickButton _shootButton;
-    private JoystickButton _resetYawButton;
-
-    private Button _driverAButton;
-    private Button _driverBButton;
-    private Button _driverXButton;
-    private Button _driverYButton;
-    private Button _driverRightTrigger;
+    protected Gamepad Gamepad;
+    protected Joystick Rotation;
+    protected Joystick Translation;
 
     private double yIn = 0;
     private double xIn = 0;
 
     public OI() {
-        _driverGamepad = new Gamepad(0);
+        Gamepad = new Gamepad(0);
 
-        _leftJoystick = new Joystick(1);
-        _rightJoystick = new Joystick(2);
-
-        _driverRightStickButton =
-                new JoystickButton(_driverGamepad, Gamepad.Buttons.RIGHT_STICK.getNumber());
-
-        _trigger = new JoystickButton(_rightJoystick, 1);
-        _thumbButton = new JoystickButton(_rightJoystick, 2);
-        _shootButton = new JoystickButton(_leftJoystick, 1);
-        _resetYawButton = new JoystickButton(_rightJoystick, 4);
-
-        _driverAButton = new JoystickButton(_driverGamepad, Gamepad.Buttons.A.getNumber());
-        _driverBButton = new JoystickButton(_driverGamepad, Gamepad.Buttons.B.getNumber());
-        _driverYButton = new JoystickButton(_driverGamepad, Gamepad.Buttons.Y.getNumber());
-        _driverXButton = new JoystickButton(_driverGamepad, Gamepad.Buttons.X.getNumber());
-        _driverRightTrigger =
-                new AxisButton(_driverGamepad, Gamepad.Axes.RIGHT_TRIGGER.getNumber(), 0.2);
+        Rotation = new Joystick(1);
+        Translation = new Joystick(2);
     }
 
-    public void initializeButtons(DriveTrain driveTrain) {}
+    public void initializeButtons(DriveTrain driveTrain) {
+        //There's nothing to init here
+    }
 
     public double getDriveY() {
-        //        yIn = getSpeedFromAxis(_leftJoystick, _leftJoystick.getYChannel());
-        yIn = getSpeedFromAxis(_driverGamepad, Gamepad.Axes.LEFT_Y.getNumber());
+        //Comment for gamepad control
+        yIn = getSpeedFromAxis(Rotation, Rotation.getYChannel());
+        //Uncomment for gamepad control
+        // yIn = getSpeedFromAxis(Gamepad, Gamepad.getYChannel());
         yIn = applyDeadband(yIn, Constants.DriveTrain.DEADBAND);
 
         double yOut = yIn / (Math.sqrt(yIn * yIn + (xIn * xIn)) + Constants.EPSILON);
@@ -67,8 +43,10 @@ public class OI extends OutliersProxy {
     }
 
     public double getDriveX() {
-        //        xIn = -getSpeedFromAxis(_leftJoystick, _leftJoystick.getXChannel());
-        xIn = -getSpeedFromAxis(_driverGamepad, Gamepad.Axes.LEFT_X.getNumber());
+        //Comment for gamepad control
+        xIn = -getSpeedFromAxis(Rotation, Rotation.getXChannel());
+        //Uncomment for gamepad control
+        //xIn = -getSpeedFromAxis(Gamepad, Gamepad.getXChannel());
         xIn = applyDeadband(xIn, Constants.DriveTrain.DEADBAND);
 
         double xOut = xIn / (Math.sqrt(yIn * yIn + (xIn * xIn)) + Constants.EPSILON);
@@ -77,7 +55,7 @@ public class OI extends OutliersProxy {
     }
 
     public double getRotationX() {
-        double speed = getSpeedFromAxis(_rightJoystick, _rightJoystick.getZChannel());
+        double speed = getSpeedFromAxis(Translation, Translation.getZChannel());
         speed = applyDeadband(speed, 0.2);
         return speed;
     }
