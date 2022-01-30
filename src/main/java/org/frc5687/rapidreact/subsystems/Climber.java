be470cb72ede171b8a0fff534e51aae30fbfc552
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2022-01-30 05:51:23 -0500

    Climber

---
+++ src/main/java/org/frc5687/rapidreact/subsystems/Climber.java
@@ -0,0 +1,13 @@
+/* Team 5687 (C)5687-2022 */
+package org.frc5687.rapidreact.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
+public class Climber extends OutliersSubsystem{
+    
+    private CANSparkMax _leftArm;
+
+    public Climber(){
+    }
+}
---
+++ src/main/java/org/frc5687/rapidreact/util/Version.java
@@ -0,0 +1,12 @@
+package org.frc5687.rapidreact.util;
+
+/**
+ * A version class to read current Git information from deployed robot code
+ * @author wilstenholme
+ */
+
+public class Version {
+    public static final String BRANCH = "Climber/#12-CreateSubsystem";
+    public static final String REVISION = "875cb1f";
+    public static final String MODIFIED = "";
+}
