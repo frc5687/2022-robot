ef1a832b374ab617141e815e53084311d2c4d8dd
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2022-01-30 06:03:40 -0500

    Base seudo code for climber

--- src/main/java/org/frc5687/rapidreact/subsystems/Climber.java
+++ src/main/java/org/frc5687/rapidreact/subsystems/Climber.java
@@ -2,12 +2,24 @@
 package org.frc5687.rapidreact.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
+
+import org.frc5687.rapidreact.util.OutliersContainer;
+
 
 public class Climber extends OutliersSubsystem{
     
     private CANSparkMax _leftArm;
+    private CANSparkMax _rightArm;
 
-    public Climber(){
+    public Climber(OutliersContainer container) {
+        super(container);
+        _leftArm = new CANSparkMax(4, CANSparkMax.MotorType.kBrushless);
+        _rightArm = new CANSparkMax(5, CANSparkMax.MotorType.kBrushless);
+    }
+
+    
+    @Override
+    public void updateDashboard() {
+
     }
 }
