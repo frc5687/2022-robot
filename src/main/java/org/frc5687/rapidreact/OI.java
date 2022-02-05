7ec205ee4a124fc7c6bea871f8ded1f02cf23cc6
BenBernardCIS <benbernard@post.harvard.edu>
2022-02-05 13:11:24 -0500

    Added JoystickButton support and more documentation.

--- src/main/java/org/frc5687/rapidreact/OI.java
+++ src/main/java/org/frc5687/rapidreact/OI.java
@@ -8,10 +8,42 @@ package org.frc5687.rapidreact;
 import static org.frc5687.rapidreact.util.Helpers.*;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
 import org.frc5687.rapidreact.subsystems.DriveTrain;
 import org.frc5687.rapidreact.util.Gamepad;
 import org.frc5687.rapidreact.util.OutliersProxy;
 
+/**
+ * To add a button to control a subsystem there are a number of steps needed.  I'll use SHOOT as an example:
+ * 1) Define the button in the ButtonMap.Buttons class:
+ * 
+ *         public static class SHOOT {
+ *           public static int Controller = Controllers.DRIVER_JOYSTICK;
+ *           public static int Button = 0;
+ *         }
+ *
+ * 2) Add a private member variable for the button:
+ * 
+ *        private JoystickButton _shootButton;
+ * 
+ * 3) Instantiate the button in the OI() constructor, referencing the new ButtonMap entry:
+ * 
+ *        _shootButton = addJoystickButton(ButtonMap.Buttons.SHOOT.Controller, ButtonMap.Buttons.SHOOT.Button);
+ * 
+ * 4) Add the subsystem to the signature for OI.initializeButtons:
+ * 
+ *        public void initializeButtons(DriveTrain driveTrain, Shooter shooter)
+ * 
+ * 5) Initialize the button in OI.initializeButtons:
+ * 
+ *        _shootButton.whenHeld(new Shoot(shooter));
+ * 
+ * 6) Add the subsystem to the RobotContainer.init() call to _oi.inializeButtons:
+ * 
+ *        _oi.initializeButtons(_driveTrain, _shooter);
+ * 
+ */
 public class OI extends OutliersProxy {
 
     private double yIn = 0;
@@ -19,14 +51,21 @@ public class OI extends OutliersProxy {
 
     private Joystick[] _joysticks = new Joystick[10]; 
 
+    // private JoystickButton _shootButton;
+
     public OI() {
         addJoystick(ButtonMap.Controllers.DRIVER_JOYSTICK);
         addJoystick(ButtonMap.Controllers.OPERATOR_JOYSTICK);
         addGamepad(ButtonMap.Controllers.OPERATOR_GAMEPAD);
+
+        // shootButton = addJoystickButton(ButtonMap.Buttons.SHOOT.Controller, ButtonMap.Buttons.SHOOT.Button);
     }
 
-    public void initializeButtons(DriveTrain driveTrain) {
+    public void initializeButtons(DriveTrain driveTrain/*, Shooter shooter*/) {
         //There's nothing to init here
+
+        // shootButton.whenHeld(new Shoot(shooter));
+
     }
 
     public double getDriveY() {
@@ -99,6 +138,11 @@ public class OI extends OutliersProxy {
     private Joystick getJoystick(int port) {
         if (port < 0) { return null; }
         return _joysticks[port];
+    }
+
+    private JoystickButton addJoystickButton(int port, int buttonNumber) {
+        Joystick joystick = getJoystick(port);
+        return new JoystickButton(joystick, buttonNumber);
     }
 
 }
