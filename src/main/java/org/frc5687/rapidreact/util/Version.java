0dab3202eedd2a196efe95dc610423cf7b5f9154
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2022-02-06 20:02:02 -0500

    Update Version.java

--- src/main/java/org/frc5687/rapidreact/util/Version.java
+++ src/main/java/org/frc5687/rapidreact/util/Version.java
@@ -7,6 +7,6 @@ package org.frc5687.rapidreact.util;
 
 public class Version {
     public static final String BRANCH = "Intake/#11-CreateSubsystemIvan";
-    public static final String REVISION = "d969f36";
+    public static final String REVISION = "297adf9";
     public static final String MODIFIED = " *";
 }
