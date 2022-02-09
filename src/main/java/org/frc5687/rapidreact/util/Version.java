0a7335a6d434f9041e9f4d95232bfbccae1a5fe1
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2022-02-09 17:21:44 -0500

    Test

--- src/main/java/org/frc5687/rapidreact/util/Version.java
+++ src/main/java/org/frc5687/rapidreact/util/Version.java
@@ -7,6 +7,10 @@ package org.frc5687.rapidreact.util;
 
 public class Version {
     public static final String BRANCH = "Catapult/#10-CreateSubsystem";
+<<<<<<< Updated upstream
     public static final String REVISION = "0b731ac";
+=======
+    public static final String REVISION = "4a60fca";
+>>>>>>> Stashed changes
     public static final String MODIFIED = " *";
 }
