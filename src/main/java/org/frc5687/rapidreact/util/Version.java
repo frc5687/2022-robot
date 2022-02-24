ff8778fd94da71935ad5a7c89a0b14bd97ec566f
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2022-02-24 14:22:22 -0500

    Update Version.java
    So I don't break something during testing.

--- src/main/java/org/frc5687/rapidreact/util/Version.java
+++ src/main/java/org/frc5687/rapidreact/util/Version.java
@@ -6,7 +6,7 @@ package org.frc5687.rapidreact.util;
  */
 
 public class Version {
-    public static final String BRANCH = "Catapult/#43-Loading";
-    public static final String REVISION = "57079ad";
+    public static final String BRANCH = "Climber/#12-CreateSubsystem";
+    public static final String REVISION = "a7252ce";
     public static final String MODIFIED = " *";
 }
