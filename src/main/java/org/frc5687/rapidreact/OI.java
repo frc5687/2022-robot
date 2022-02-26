3d8d5fd65dde205120e9ecb12857da4755f2e934
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2022-02-26 16:56:32 -0500

    Reformat

--- src/main/java/org/frc5687/rapidreact/OI.java
+++ src/main/java/org/frc5687/rapidreact/OI.java
@@ -64,8 +64,6 @@ public class OI extends OutliersProxy {
 //        _release.whenPressed(catapult::releaseArm);
 //        _setState.whenPressed(new SetState(catapult, Catapult.CatapultState.AIMING));
 //        _dropArm.whenHeld(new Feed(servoStop));
-
-
         _stow.whenPressed(new Stow(climber));
         _prepClimb.whenPressed(new PrepToClimb(climber));
 
