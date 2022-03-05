02ac87df7a27ff06b1046113a442042718a06696
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2022-03-05 12:55:50 -0500

    Update Lights.java

--- src/main/java/org/frc5687/rapidreact/subsystems/Lights.java
+++ src/main/java/org/frc5687/rapidreact/subsystems/Lights.java
@@ -2,21 +2,22 @@ package org.frc5687.rapidreact.subsystems;
 
 import org.frc5687.rapidreact.Constants;
 import org.frc5687.rapidreact.util.OutliersContainer;
-
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 public class Lights extends OutliersSubsystem{
 
     private Spark _blinkens;
+    private boolean _running;
     
     public Lights(OutliersContainer container, int port) {
         super(container);
-        _blinkens = new Spark(port);
-        _blinkens.set(Constants.Blinkens.CONFETTI);
+        _blinkens = new Spark(8);
+        _running = true;
+        _blinkens.set(0.87);
     }
 
     @Override
     public void updateDashboard() {
-        metric("Blikens done", true);
+        metric("Blikens done", _running);
     }
 }
