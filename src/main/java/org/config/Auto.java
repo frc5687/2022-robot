4d37ad83e9b632d60fa8cc7ec59d5e84e4fc5bbb
Fred Horch <fred.horch@gmail.com>
2022-03-07 16:52:34 -0500

    Added config folder and Auto class

---
+++ src/main/java/org/config/Auto.java
@@ -0,0 +1,34 @@
+package org.config;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class Auto {
+        
+    public static class FieldPositions {
+        public static Pose2d SAFE_BALL_THREE = new Pose2d(7.7, 0.8, new Rotation2d());
+        public static Pose2d ROBOT_POS_ONE_DEST = new Pose2d(6.505, 5.685, new Rotation2d());
+        public static Pose2d ROBOT_POS_TWO_DEST = new Pose2d(4.63, 3.65, new Rotation2d());
+        public static Pose2d ROBOT_POS_THREE_DEST = new Pose2d(5.95, 1.42, new Rotation2d());
+        public static Pose2d PARALLEL_PARK = new Pose2d(8.42, 0.4, new Rotation2d());
+
+    }
+    public static class BallPositions {
+        public static Pose2d BALL_ONE = new Pose2d(4.755, 6.085, new Rotation2d());
+        public static Pose2d BALL_TWO = new Pose2d(5.1, 1.77, new Rotation2d());
+        public static Pose2d BALL_THREE = new Pose2d(7.7, 0.28, new Rotation2d());
+    }
+
+    public static class RobotPositions {
+        public static Pose2d FIRST = new Pose2d(6.46, 5.45, new Rotation2d());
+        public static Pose2d SECOND = new Pose2d(5.9, 3.93, new Rotation2d());
+        public static Pose2d THIRD = new Pose2d(6.9, 2.5, new Rotation2d());
+        public static Pose2d FOURTH = new Pose2d(8.25, 1.9, new Rotation2d());
+    }
+
+    public static class Rotations {
+        public static Rotation2d BALL_THREE_FROM_FOURTH = new Rotation2d(0.5 * Math.PI);
+    }
+
+    public static final double DRIVETRAIN_POWER = 0.5;
+}
