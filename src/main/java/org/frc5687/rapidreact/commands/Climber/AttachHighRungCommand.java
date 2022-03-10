789b476b3de4116696ce969103c2dc02c98216b3
BenBernardCIS <benbernard@post.harvard.edu>
2022-03-09 22:41:09 -0500

    Added delay between attach high and attach traverse.  Currently set to 1/2 second, defined by Constants.Climber.ROCKER_PISTON_SETTLE

--- src/main/java/org/frc5687/rapidreact/commands/Climber/AttachHighRungCommand.java
+++ src/main/java/org/frc5687/rapidreact/commands/Climber/AttachHighRungCommand.java
@@ -64,6 +64,19 @@ public class AttachHighRungCommand extends OutliersCommand{
                 info("AttachHighRungCommand advancing to WAIT_STATIONARY step.");
                 break;
             case WAIT_STATIONARY:
+                if (_climber.isStaAtGoal()) {
+                    _step = Step.WAIT_SETTLE;
+                    _waitUntil = System.currentTimeMillis() + Constants.Climber.ROCKER_PISTON_SETTLE;
+                    info("AttachHighRungCommand advancing to WAIT_SETTLE step.");
+                }
+                break;
+            case WAIT_SETTLE:
+                if ( System.currentTimeMillis() > _waitUntil) {
+                    _step = Step.DONE;
+                    info("AttachHighRungCommand advancing to DONE step.");
+                }
+                break;
+            case DONE:
                 break;
         }
 
@@ -73,7 +86,7 @@ public class AttachHighRungCommand extends OutliersCommand{
     @Override
     public boolean isFinished(){
         super.isFinished();
-        if (_step == Step.WAIT_STATIONARY && _climber.isStaAtGoal()) {
+        if (_step == Step.DONE) {
             _climber.setStep(Climber.ClimberStep.ATTACHED_HIGH);
             info("Finished AttachHighRungCommand.");
             return true;
@@ -94,7 +107,9 @@ public class AttachHighRungCommand extends OutliersCommand{
         RETRACT_ROCKER(3),
         WAIT_ROCKER(4),
         EXTEND_STATIONARY(5),
-        WAIT_STATIONARY(6);
+        WAIT_STATIONARY(6),
+        WAIT_SETTLE(7),
+        DONE(8);
 
         private final int _value;
         Step(int value) { 
