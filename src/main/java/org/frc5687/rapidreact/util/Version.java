38b7e163c9db057d3f8007b990ae16c3073011d5
deslobodzian <de.slobodzian@gmail.com>
2022-03-12 13:17:38 -0500

    Removed color sensor added angle to servo

--- src/main/java/org/frc5687/rapidreact/util/Version.java
+++ src/main/java/org/frc5687/rapidreact/util/Version.java
@@ -7,6 +7,6 @@ package org.frc5687.rapidreact.util;
 
 public class Version {
     public static final String BRANCH = "Events/PinetreeBen";
-    public static final String REVISION = "d13ec95";
+    public static final String REVISION = "2a391e0";
     public static final String MODIFIED = " *";
 }
