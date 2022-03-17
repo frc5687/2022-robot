6d8f8d75332042933cf65b11589d92b4dfbd55d0
jackellingwood <jack.ellingwood@baxter-academy.org>
2022-03-17 17:08:40 -0400

    fixed some positions

--- src/main/java/org/frc5687/rapidreact/config/Auto.java
+++ src/main/java/org/frc5687/rapidreact/config/Auto.java
@@ -8,7 +8,7 @@ public class Auto {
         
     public static class FieldPositions {
         public static Pose2d SAFE_BALL_THREE = new Pose2d(7.7, 0.8, new Rotation2d());
-        public static Pose2d SAFE_BALL_FOUR = new Pose2d(1.49, 1.25, new Rotation2d());
+        public static Pose2d SAFE_BALL_FOUR = new Pose2d(1.25, 1.49, new Rotation2d());
         public static Pose2d ROBOT_POS_ONE_DEST = new Pose2d(6.505, 5.685, new Rotation2d());
         public static Pose2d ROBOT_POS_TWO_DEST = new Pose2d(4.63, 3.65, new Rotation2d());
         public static Pose2d ROBOT_POS_THREE_DEST = new Pose2d(5.95, 1.42, new Rotation2d());
