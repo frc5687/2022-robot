b78eaa724120b480f05bed292a7d713df6d853b1
SuperSaurus1 <simeon.pillsbury@gmail.com>
2022-03-23 22:07:02 -0400

    Removed unnecessaries, added isFinished()

--- src/main/java/org/frc5687/rapidreact/commands/ToggleGate.java
+++ src/main/java/org/frc5687/rapidreact/commands/ToggleGate.java
@@ -1,11 +1,10 @@
 package org.frc5687.rapidreact.commands;
 
 import org.frc5687.rapidreact.subsystems.Catapult;
-import org.frc5687.rapidreact.util.ServoStop;
 
 public class ToggleGate extends OutliersCommand {
     private Catapult _catapult;
-    private ServoStop servoStop;
+    private boolean _done = false;
 
     public ToggleGate(Catapult catapult){
         _catapult = catapult;
@@ -17,10 +16,26 @@ public class ToggleGate extends OutliersCommand {
 
         if (_catapult.getGateState()){ // If indexer is up, moves it down
                 _catapult.lowerGate();
+                _done = true;
         }
 
         else if (!_catapult.getGateState()){ // If indexer is down, moves it up
                 _catapult.raiseGate();
+                _done = true;
         }
     }
+
+    @Override
+    public boolean isFinished(){
+        if (_done && _catapult.getGateState()){ // If indexer has been moved up, returns true
+            info("Gate has been raised");
+            return true;
+        }
+        else if (_done && !_catapult.getGateState()) { // If indexer has been moved down, returns true
+            info("Gate has been lowered");
+            return true;
+        }
+            return false; // If indexer is not at either position, or has not been moved
+    }
+
 }
