1996473d62d5dddb7b7feb44cd11a50defb85768
jackellingwood <jack.ellingwood@baxter-academy.org>
2022-10-14 17:33:47 -0400

    2 gamepad style, with arbitrary buttons removed

--- src/main/java/org/frc5687/rapidreact/util/Version.java
+++ src/main/java/org/frc5687/rapidreact/util/Version.java
@@ -7,6 +7,6 @@ package org.frc5687.rapidreact.util;
 
 public class Version {
     public static final String BRANCH = "OI/#151b-NewDriveWithGamepad";
-    public static final String REVISION = "1e74ce8";
+    public static final String REVISION = "903d148";
     public static final String MODIFIED = " *";
 }
